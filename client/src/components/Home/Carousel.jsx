8c36f0110f1c173f168d1c92805c3e367ae734d7
atdzhiev <atdjiev@abv.bg>
2025-12-09 13:45:29 +0200

    Carousel changed

--- client/src/components/Home/Carousel.jsx
+++ client/src/components/Home/Carousel.jsx
@@ -1,136 +1,106 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router";
+import { ProductsContext } from "../../contexts/ProductsContext";
+import "./Carousel.css"; 
+
 export default function Carousel() {
-    return ( <div
-  id="template-mo-zay-hero-carousel"
-  className="carousel slide"
-  data-bs-ride="carousel"
->
-  <ol className="carousel-indicators">
-    <li
-      data-bs-target="#template-mo-zay-hero-carousel"
-      data-bs-slide-to={0}
-      className="active"
-    />
-    <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to={1} />
-    <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to={2} />
-  </ol>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <div className="container">
-        <div className="row p-5">
-          <div className="mx-auto col-md-8 col-lg-6 order-lg-last">
-            <img
-              className="img-fluid"
-              src="./assets/img/banner_img_01.jpg"
-              alt=""
-            />
-          </div>
-          <div className="col-lg-6 mb-0 d-flex align-items-center">
-            <div className="text-align-left align-self-center">
-              <h1 className="h1 text-success">
-                <b>Zay</b> eCommerce
-              </h1>
-              <h3 className="h2">Tiny and Perfect eCommerce Template</h3>
-              <p>
-                Zay Shop is an eCommerce HTML5 CSS template with latest version
-                of Bootstrap 5 (beta 1). This template is 100% free provided by{" "}
-                <a
-                  rel="sponsored"
-                  className="text-success"
-                  href="https://templatemo.com"
-                  target="_blank"
-                >
-                  TemplateMo
-                </a>{" "}
-                website. Image credits go to{" "}
-                <a
-                  rel="sponsored"
-                  className="text-success"
-                  href="https://stories.freepik.com/"
-                  target="_blank"
-                >
-                  Freepik Stories
-                </a>
-                ,
-                <a
-                  rel="sponsored"
-                  className="text-success"
-                  href="https://unsplash.com/"
-                  target="_blank"
-                >
-                  Unsplash
-                </a>{" "}
-                and
-                <a
-                  rel="sponsored"
-                  className="text-success"
-                  href="https://icons8.com/"
-                  target="_blank"
-                >
-                  Icons 8
-                </a>
-                .
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <div className="container">
-        <div className="row p-5">
-          <div className="mx-auto col-md-8 col-lg-6 order-lg-last">
-            <img
-              className="img-fluid"
-              src="./assets/img/banner_img_02.jpg"
-              alt=""
-            />
-          </div>
-          <div className="col-lg-6 mb-0 d-flex align-items-center">
-            <div className="text-align-left">
-              <h1 className="h1">Proident occaecat</h1>
-              <h3 className="h2">Aliquip ex ea commodo consequat</h3>
-              <p>
-                You are permitted to use this Zay CSS template for your
-                commercial websites. You are <strong>not permitted</strong> to
-                re-distribute the template ZIP file in any kind of template
-                collection websites.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <div className="container">
-        <div className="row p-5">
-          <div className="mx-auto col-md-8 col-lg-12 order-lg-last">
-            <img
-              className="row align-items-center py-5"
-              src="./assets/img/BannerPen.jpg"
-              alt=""
-            />
-          </div>
+  const navigate = useNavigate();
+  const { onBrandClickHandler } = useContext(ProductsContext);
+
+  const handleBrandClick = (brand) => {
+    onBrandClickHandler(brand);
+    navigate("/products");
+  };
+
+  return (
+    <div
+      id="template-mo-zay-hero-carousel"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+    
+      <ol className="carousel-indicators">
+        <li
+          data-bs-target="#template-mo-zay-hero-carousel"
+          data-bs-slide-to={0}
+          className="active"
+        />
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to={1} />
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to={2} />
+      </ol>
+ 
+      <div className="carousel-inner">
+       
         
+        <div className="carousel-item">
+          <div className="container">
+            <div className="row p-5">
+              <div
+                className="banner banner-1"
+              >
+                <div className="banner-content left">
+                  <h2>World of Parker</h2>
+                  <p>
+                    Explore over 130 years of Parker brand history, expert craftsmanship
+                    and the future of Parker fine pens.
+                  </p>
+                  <button onClick={() => handleBrandClick("Parker")}>
+                    Describe
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+        <div className="carousel-item active">
+          <div className="container">
+            <div className="row p-5">
+              <div className="banner banner-2">
+                <div className="banner-content right">
+                  <h2>Discover State</h2>
+                  <p>Your Stationery Destination</p>
+                  <p>Step into a world where creativity meets craftsmanship.</p>
+                  <button onClick={() => navigate("/about")}>About Us</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+       
+        <div className="carousel-item">
+          <div className="container">
+            <div className="row p-5">
+              <div className="banner banner-3">
+                <h2>Explore our environmentally friendly office supplies</h2>
+                <button onClick={() => handleBrandClick("Navigator")}>
+                  Explore
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+     
+      <a
+        className="carousel-control-prev text-decoration-none w-auto ps-3"
+        href="#template-mo-zay-hero-carousel"
+        role="button"
+        data-bs-slide="prev"
+      >
+        <i className="fas fa-chevron-left" />
+      </a>
+      <a
+        className="carousel-control-next text-decoration-none w-auto pe-3"
+        href="#template-mo-zay-hero-carousel"
+        role="button"
+        data-bs-slide="next"
+      >
+        <i className="fas fa-chevron-right" />
+      </a>
     </div>
-  </div>
-  <a
-    className="carousel-control-prev text-decoration-none w-auto ps-3"
-    href="#template-mo-zay-hero-carousel"
-    role="button"
-    data-bs-slide="prev"
-  >
-    <i className="fas fa-chevron-left" />
-  </a>
-  <a
-    className="carousel-control-next text-decoration-none w-auto pe-3"
-    href="#template-mo-zay-hero-carousel"
-    role="button"
-    data-bs-slide="next"
-  >
-    <i className="fas fa-chevron-right" />
-  </a>
-</div>
-)
+  );
 }
